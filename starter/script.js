528a8288c53d527f48b8a42027dac535033e61ee
manoy96 <mfewcar@gmail.com>
2019-09-16 17:46:52 -0600

    operator precedence

--- starter/script.js
+++ starter/script.js
@@ -47,30 +47,70 @@
  * Basic operators
  */
 
- var year, yearManuel, yearMark;
- now = 2019;
- ageManuel = 23;
- ageMark = 30;
+//  var year, yearManuel, yearMark;
+//  now = 2019;
+//  ageManuel = 23;
+//  ageMark = 30;
 
-//  Math operators
- yearManuel = now - 23;
- yearMark = now - 33;
+// //  Math operators
+//  yearManuel = now - 23;
+//  yearMark = now - 33;
 
 
- console.log(yearManuel);
+//  console.log(yearManuel);
 
- console.log(now + 2);
- console.log(now * 2);
- console.log(now / 10);
+//  console.log(now + 2);
+//  console.log(now * 2);
+//  console.log(now / 10);
 
-//Logical operators
-var manuelOlder = ageManuel > ageMark;
-var manuelOlder = ageManuel < ageMark;
-console.log(manuelOlder);
+// //Logical operators
+// var manuelOlder = ageManuel > ageMark;
+// var manuelOlder = ageManuel < ageMark;
+// console.log(manuelOlder);
 
-//typeof operator
-console.log(typeof manuelOlder);
-console.log(typeof ageMark);
-console.log(typeof 'Manuel is younger than Mark');
-var x;
-console.log(typeof x);
+// //typeof operator
+// console.log(typeof manuelOlder);
+// console.log(typeof ageMark);
+// console.log(typeof 'Manuel is younger than Mark');
+// var x;
+// console.log(typeof x);
+
+
+
+/*****************
+ * Operator precedence
+ */
+
+var now = 2019;
+var yearManuel = 1996;
+var fullAge = 23;
+
+// Multiple operators
+var isFullAge = now - yearManuel >= fullAge; // true
+console.log(isFullAge);
+
+// Grouping
+var ageManuel = now - yearManuel;
+var ageMark = 35;
+var average = (ageManuel + ageMark) / 2;
+console.log(average);
+
+//Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+console.log(x, y);
+
+/***************
+ * More operators
+ */
+
+// x = x * 2;
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+
+// x = x +1;
+// x += 1;
+x--;
+console.log(x);
