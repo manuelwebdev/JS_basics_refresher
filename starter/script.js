74a735bd974f6a3405ca11cbdf8c35dd6ad79ea7
manoy96 <mfewcar@gmail.com>
2019-09-15 23:49:15 -0600

    practice with variables, and var mutations

--- starter/script.js
+++ starter/script.js
@@ -25,3 +25,19 @@ var fName = 'manuel';
 var age = 23;
 
 console.log(fName + ' ' + age);
+
+var job, isMarried;
+job =  'teacher';
+isMarried = false;
+
+console.log(fName + ' is a ' + age + ' years old ' + job + '. Is he married?' +isMarried);
+
+/* Variable mutation */
+
+age = 'twenty three';
+job = 'driver';
+
+alert(fName + ' is a ' + age + ' years old ' + job + '. Is he married? ' +isMarried);
+
+var lName = prompt('What is your last name?');
+console.log(fName + ' ' + lName);
