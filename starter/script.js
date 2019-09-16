f869701ea6ef9190038d1820a1024a895e4b1d83
manoy96 <mfewcar@gmail.com>
2019-09-15 23:39:26 -0600

    initial

---
+++ starter/script.js
@@ -0,0 +1,27 @@
+/*****************
+* Variables and data types
+*/
+// var fName = 'manuel';
+// console.log(fName);
+
+// var lName = 'espinoza';
+// var age = 23;
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job;
+// console.log(job);
+
+// job = 'teacher';
+// console.log(job);
+
+
+/*****************
+* Variable mutations and type coercion
+*/
+
+var fName = 'manuel';
+var age = 23;
+
+console.log(fName + ' ' + age);
