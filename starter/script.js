d843e9915d0c14ea123de80064d6563b37e881e2
manoy96 <mfewcar@gmail.com>
2019-09-18 15:58:15 -0600

    truthy falsy values

--- starter/script.js
+++ starter/script.js
@@ -255,3 +255,44 @@ switch (true) {
         console.log(fName + ' is a man.');
         break;
 }
+
+
+/********************
+ * Truthy and Falsy values and equality operators
+ */
+
+ // falsy values: undefined, null, 0, '', NaN
+ // truthy values: NOT falsy values
+
+ var height;
+ height = 0;
+
+ if (height || height === 0) {
+     console.log('Variable is defined');
+ } else {
+     console.log('Variable has NOT been defined');
+ }
+
+
+// Equality operators
+if (height == '23') {
+    console.log('The == operator does type coercion!');
+} else {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
