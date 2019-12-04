50ae94dace8b3696e26f82c491e19a5c2a55bfea
manoy96 <mfewcar@gmail.com>
2019-12-04 12:52:25 -0700

    working on coding challege 3

--- starter/script.js
+++ starter/script.js
@@ -394,18 +394,46 @@ console.log(whatDoYouDo('janitor', 'Jane'));
  * Arrays
  */
 
- var names = ['John', 'Mark', 'Jane'];
- var years = new Array(1996, 1969, 1948);
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1996, 1969, 1948);
 
- console.log(names.length);
+console.log(names.length);
 
 // Mutate array data
- names[1] = 'Ben';
- names[names.length] = 'Mary';
- console.log(names);
+names[1] = 'Ben';
+names[names.length] = 'Mary';
+console.log(names);
 
- // Different data types
- var john = ['John', 'Smith', 1990, 'teacher', false];
+// Different data types
+var john = ['John', 'Smith', 1990, 'teacher', false];
 
- john.push('blue');
- console.log(john);
+john.push('blue');
+john.unshift('Mr.');
+console.log(john);
+
+john.pop();
+john.pop();
+john.shift();
+console.log(john);
+
+console.log(john.indexOf(1990));
+
+var isDesigner = john.indexOf('designer') === -1 ? 'john is NOT a designer' : "John IS a designer";
+console.log(isDesigner);
+
+
+
+
+
+
+
+
+/******************************
+ * Coding Challenge 3
+ */
+
+ var bill = [124, 48, 268];
+
+ function tipCalculator(bill) {
+     
+ }
